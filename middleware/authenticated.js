62d4b15174bd5627558fe7c4a29886d1480713ac
siYuanJun <2039750417@qq.com>
2021-09-15 12:36:46 +0800

    test: commitlinet

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -4,6 +4,7 @@ export default ({ app, store, redirect }) => {
     const loginId = encrytion.aesDecrypt(app.$cookies.get(app.$config.loginKey))
     // If the user is not authenticated
     store.commit('setLoginId', loginId)
+
     if (!store.state.loginId) {
         return redirect(app.i18n.path('user/login'))
     }
